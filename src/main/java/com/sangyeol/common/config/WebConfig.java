8cb75a006ef7cda404e1e20fd7c5746ad33303b5
xxbeann <yjb4945@gmail.com>
2026-08-26 09:43:41 +0900

    fix: change method signature

--- src/main/java/com/sangyeol/common/config/WebConfig.java
+++ src/main/java/com/sangyeol/common/config/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**")
-                .allowedOrigins(
+                .allowedOriginPatterns(
                         "http://localhost:3000",
                         "https://sangyeol-fe-web-*.vercel.app")
                 .allowedMethods("GET");
